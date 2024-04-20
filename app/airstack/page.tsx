24027ac04a11bc1587eaf1cb848f649bf6fb9fe7
Esteban Carrera <127622233+EstebanCarr@users.noreply.github.com>
2024-04-20 12:18:20 -0600

    aristik

--- app/airstack/page.tsx
+++ app/airstack/page.tsx
@@ -1,7 +1,66 @@
-import { config } from "dotenv";
+'use client'
+import { gql, GraphQLClient } from 'graphql-request';
+import { useState, useEffect } from 'react';
+import { init, useQuery } from "@airstack/airstack-react";
+const AIRSTACK_API_URL = 'https://api.airstack.xyz/graphql';
+const AIRSTACK_API_KEY = process.env.AIRSTACK_API_KEY;
 
-config();
-
-export default function aristack() {
-    return <div>¡Bienvenido aristack !</div>;
+const query = gql`
+  query MyQuery {
+    Wallet(input: { identity: "vitalik.eth", blockchain: ethereum }) {
+      socials {
+        dappName
+        profileName
+      }
+      addresses
+    }
   }
+`;
+
+
+interface WalletData {
+  socials: {
+    dappName: string;
+    profileName: string;
+  }[];
+  addresses: string[];
+}
+
+export default function AirstackPage() {
+  const [data, setData] = useState<WalletData | null>(null);
+  useEffect(() => {
+  async function fetchData() {
+    if (!AIRSTACK_API_KEY) {
+      console.error('AIRSTACK_API_KEY is not set');
+      return;
+    }
+
+    const graphQLClient = new GraphQLClient(AIRSTACK_API_URL, {
+      headers: {
+        Authorization: AIRSTACK_API_KEY ? `Bearer ${AIRSTACK_API_KEY}` : '',
+      },
+    });
+
+    try {
+      const data = await graphQLClient.request<{ Wallet: WalletData }>(query);
+      setData(data.Wallet);
+        console.log(data);
+      
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  fetchData();
+}, []);
+  return (
+    <div>
+    <h1>Integración de Airstack API en Next.js</h1>
+    {data ? (
+      <h1>{data.addresses[0]}</h1>
+    ) : (
+      <p>Cargando...</p>
+    )}
+  </div>
+  );
+}
--- app/zora/page.tsx
+++ app/zora/page.tsx
@@ -1,3 +1,6 @@
+import { config } from "dotenv";
+
+config();
 export default function zora() {
     return <div>¡Bienvenido zora !</div>;
   }
